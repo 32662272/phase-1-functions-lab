a71c65351a2e6fc5f4cb8f2cbaa0d727831fab41
Max <maxmwangikenya@gmail.com>
2023-03-16 15:23:06 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickuplocation) {
+  let hqlocation = 42;
+  return Math.abs(pickuplocation - hqlocation);
+}
+
+function distanceFromHqInFeet(pickuplocation){
+  const feetperBlock = 264
+  const blocks = distanceFromHqInBlocks(pickuplocation);
+  return (blocks * feetperBlock);
+}
+function distanceTravelledInFeet(start, stop) {
+const feetPerBlock = 264;
+const distanceTravelled = Math.abs(stop - start);
+return distanceTravelled * feetPerBlock;
+}    function calculatesFarePrice(startB, stopB) {
+const distanceInFeet = distanceTravelledInFeet(startB, stopB)
+if (distanceInFeet <= 400) {
+    return 0;
+}
+else if (distanceInFeet >= 400 && distanceInFeet <= 2000) {
+    return (distanceInFeet - 400) * 0.02;
+}
+else if (distanceInFeet >= 2000 && distanceInFeet <= 2500) {
+    return 25;
+}
+else {
+    return `cannot travel that far`;
+}
+}
